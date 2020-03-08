1ce71330f9a7f83e22339df628b66931d40a8f97
Sokolouskaya <epogrosheva@mail.com>
2020-03-09 00:50:19 +0300

    feat: implement brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsMapping = bracketsConfig.reduce((acc, [opened, closed]) => ({...acc, [closed]: opened}), {});
+    const bracketsDuplicate = new Set(
+        bracketsConfig
+            .filter(([opened, closed]) => opened === closed)
+            .map(([opened]) => opened)
+    );
+
+    const stack = [];
+
+    for (let bracket of str) {
+        const bracketOpposite = bracketsMapping[bracket];
+
+        if (!bracketOpposite) {
+            stack.push(bracket);
+        } else {
+            const last = stack.pop();
+
+            if (last !== bracketOpposite) {
+                if (bracketsDuplicate.has(bracketOpposite)) {
+                    if (last) {
+                        stack.push(last);
+                    }
+
+                    stack.push(bracket);
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
